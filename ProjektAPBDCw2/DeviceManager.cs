c7e6c731483ce9f5a2ce2a618730a8c514a449fa
TomaszMez <tomtablet123456@gmail.com>
2026-03-19 23:00:27 +0100

    Dodanie klasy DeviceManager pozwalajacej przechowywac i zarzadzac urzadzeniami

---
+++ ProjektAPBDCw2/DeviceManager.cs
@@ -0,0 +1,59 @@
+﻿namespace ProjektAPBDCw2;
+
+
+public static class UserManager
+{
+    public static List<Device> Devices { get; } = new List<Device>();
+
+    public static Device StringToDevice(String inputStr)
+    {
+        string[] fields = inputStr.Split(' ');
+        
+        int m = -1;
+        foreach (Manufacturer manufacturer in Enum.GetValues(typeof(Manufacturer)))
+        {
+            if (fields[2].Equals(manufacturer.ToString()))
+            {
+                m = (int) manufacturer;
+            }
+        }
+        if (m == -1) throw new Exception("Invalid input");
+        
+        switch  (fields[0])
+        {
+            case "Laptop":
+                if (fields.Length != 6) throw new Exception("Invalid input");
+                return new Laptop(fields[1], (Manufacturer) m, int.Parse(fields[3]), float.Parse(fields[4]), bool.Parse(fields[5]));
+            case "Camera":
+                if (fields.Length != 6) throw new Exception("Invalid input");
+                return new Camera(fields[1], (Manufacturer) m, int.Parse(fields[3]), int.Parse(fields[4]), bool.Parse(fields[5]));
+            case "Projector":
+                if (fields.Length != 6) throw new Exception("Invalid input");
+                return new Projector(fields[1], (Manufacturer) m, int.Parse(fields[3]), int.Parse(fields[4]), int.Parse(fields[5]));
+            default:
+                throw new Exception("Invalid input");
+        }
+    }
+
+    public static Device GetDeviceById(int id)
+    {
+        foreach (Device device in Devices)
+        {
+            if (device.Id == id) return device;
+        }
+        throw new Exception("Device not found");
+    }
+    
+    public static void RemoveDeviceById(int id)
+    {
+        foreach (Device device in Devices)
+        {
+            if (device.Id == id)
+            {
+                Devices.Remove(device);
+                return;
+            }
+        }
+    }
+    
+}
