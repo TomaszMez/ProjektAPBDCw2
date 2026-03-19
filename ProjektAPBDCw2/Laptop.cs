22c273c473607e370242459fe4b9a0f1ab3ab467
TomaszMez <tomtablet123456@gmail.com>
2026-03-19 22:13:05 +0100

    Dodanie klas Laptop Camera Projector dziedziczacych po Device reprezentujacych rozne typy urzadzen i ich unikalne wlasciwosci

---
+++ ProjektAPBDCw2/Laptop.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjektAPBDCw2;
+
+public class Laptop : Device
+{
+    public float BatteryTime { get; }
+    public bool HasWebcam { get; }
+
+    public Laptop(string name, Manufacturer manufacturer, int price, float batteryTime, bool hasWebcam) : base(name, manufacturer, price)
+    {
+        BatteryTime = batteryTime;
+        HasWebcam = hasWebcam;
+    }
+}
---
+++ ProjektAPBDCw2/Projector.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjektAPBDCw2;
+
+public class Projector : Device
+{
+    public int Brightness { get; }
+    public int MaxRange { get; }
+    
+    public Projector(string name, Manufacturer manufacturer, int price, int brightness, int maxRange) : base(name, manufacturer, price)
+    {
+        Brightness = brightness;
+        MaxRange = maxRange;
+    }
+}
