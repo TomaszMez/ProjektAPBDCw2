f0f53a055629b5e479b08b6f3062915f4af5c4b5
TomaszMez <tomtablet123456@gmail.com>
2026-03-19 22:10:24 +0100

    Dodanie klasy abstrakcyjnej Device bazowej dla roznych typow urzadzen wraz z enumem Manufacturer mowiacym o producencie urzadzenia

---
+++ ProjektAPBDCw2/Device.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjektAPBDCw2;
+
+public abstract class Device
+{
+    private static int _idCounter = 0;
+    
+    public int Id { get; }
+    public string Name { get; }
+    public Manufacturer Manufacturer { get; }
+    public int Price { get; }
+    public bool Available { get; set; }
+    protected Device(string name, Manufacturer manufacturer, int price) 
+    {
+        Id = _idCounter++;
+        Name = name;
+        Manufacturer = manufacturer;
+        Price = price;
+        
+        Available = true;
+    }
+}
---
+++ ProjektAPBDCw2/Manufacturer.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjektAPBDCw2;
+
+public enum Manufacturer
+{
+    Sony,
+    Panasonic,
+    Acer,
+    Asus
+}
+
+public static class ManufacturerExtensions
+{
+    public static string ToString(this Manufacturer manufacturer)
+    {
+        return manufacturer switch
+        {
+            Manufacturer.Sony => "Sony",
+            Manufacturer.Panasonic => "Panasonic",
+            Manufacturer.Acer => "Acer",
+            Manufacturer.Asus => "Asus",
+            _ => throw new ArgumentOutOfRangeException(nameof(manufacturer), manufacturer, null)
+        };
+    }
+}
