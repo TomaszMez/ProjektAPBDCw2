5d7480a7544e243b99af5f14ce2ebfee46358b34
TomaszMez <tomtablet123456@gmail.com>
2026-03-19 22:38:01 +0100

    Dodanie klasy Rental pozwalajacej na sledzenie wypozyczen uzytkownikow

---
+++ ProjektAPBDCw2/Rental.cs
@@ -0,0 +1,34 @@
+﻿namespace ProjektAPBDCw2;
+
+public class Rental
+{
+    public int UserId { get; }
+    public int DeviceId { get; }
+    public DateTime RentalDate { get; }
+    public DateTime AgreedReturnDate { get; }
+    public DateTime? ActualReturnDate { get; private set; }
+    public bool? IsOnTime { get; private set; }
+    public int? Penalty { get; private set; }
+
+    public Rental(int userId, int devideId, DateTime rentalDate, DateTime agreedReturnDate)
+    {
+        UserId = userId;
+        DeviceId = devideId;
+        RentalDate = rentalDate;
+        AgreedReturnDate = agreedReturnDate;
+    }
+
+    public void ReturnDevice()
+    {
+        ActualReturnDate = DateTime.Now;
+        if (ActualReturnDate > AgreedReturnDate)
+        {
+            IsOnTime = false;
+            TimeSpan? diff = ActualReturnDate-AgreedReturnDate;
+            Penalty = diff.Value.Days*100;
+            
+        }
+        else IsOnTime = true;
+    }
+    
+}
