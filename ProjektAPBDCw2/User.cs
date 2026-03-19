787911b7a6f60136aa353e41774815b8db83ee19
TomaszMez <tomtablet123456@gmail.com>
2026-03-19 22:15:23 +0100

    Dodanie klasy User reprezentujacej uzytkownika wraz z enumem UserType mowiacym o typie uzytkownika

---
+++ ProjektAPBDCw2/User.cs
@@ -0,0 +1,32 @@
+﻿namespace ProjektAPBDCw2;
+
+public class User
+{
+    private static int _idCounter = 0;
+    
+    public int Id { get; }
+    public string Name { get; }
+    public string Surname { get; }
+    public UserType UserType { get; }
+
+    public User(string name, string surname, UserType userType)
+    {
+        Id = _idCounter++;
+        Name = name;
+        Surname = surname;
+        UserType = userType;
+    }
+
+    public User(User other)
+    {
+        Id = other.Id;
+        Name = other.Name;
+        Surname = other.Surname;
+        UserType = other.UserType;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id} {Name} {Surname} {UserType}";
+    }
+}
---
+++ ProjektAPBDCw2/UserType.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjektAPBDCw2;
+
+public enum UserType
+{
+    Student,
+    Employee
+}
+
+public static class UserTypeExtensions
+{
+    public static string ToString(this UserType userType)
+    {
+        return userType switch
+        {
+            UserType.Student => "Student",
+            UserType.Employee => "Employee",
+            _ => throw new ArgumentOutOfRangeException(nameof(userType), userType, null)
+        };
+    }
+}
