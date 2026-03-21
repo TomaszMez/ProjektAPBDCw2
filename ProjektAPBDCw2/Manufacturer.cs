e61ea9a15c05928a12bf4f7e597dfb943340ea09
TomaszMez <tomtablet123456@gmail.com>
2026-03-21 13:58:35 +0100

    Usuniecie rozszerzen z ToString dla enumow

--- ProjektAPBDCw2/Manufacturer.cs
+++ ProjektAPBDCw2/Manufacturer.cs
@@ -7,18 +7,3 @@ public enum Manufacturer
     Acer,
     Asus
 }
-
-public static class ManufacturerExtensions
-{
-    public static string ToString(this Manufacturer manufacturer)
-    {
-        return manufacturer switch
-        {
-            Manufacturer.Sony => "Sony",
-            Manufacturer.Panasonic => "Panasonic",
-            Manufacturer.Acer => "Acer",
-            Manufacturer.Asus => "Asus",
-            _ => throw new ArgumentOutOfRangeException(nameof(manufacturer), manufacturer, null)
-        };
-    }
-}
--- ProjektAPBDCw2/UserType.cs
+++ ProjektAPBDCw2/UserType.cs
@@ -5,16 +5,3 @@ public enum UserType
     Student,
     Employee
 }
-
-public static class UserTypeExtensions
-{
-    public static string ToString(this UserType userType)
-    {
-        return userType switch
-        {
-            UserType.Student => "Student",
-            UserType.Employee => "Pracownik",
-            _ => throw new ArgumentOutOfRangeException(nameof(userType), userType, null)
-        };
-    }
-}
