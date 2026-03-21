c11a8c0b3398fe45c9f574f00b420a2c2c97552a
TomaszMez <tomtablet123456@gmail.com>
2026-03-21 13:57:50 +0100

    Wiadomosci wyjatkow w metodach parsujacych ze String wskazuja teraz na przyczyne wyjatku

--- ProjektAPBDCw2/InputParser.cs
+++ ProjektAPBDCw2/InputParser.cs
@@ -5,7 +5,7 @@ public static class InputParser
     public static User StringToUser(String inputStr)
     {
         string[] fields = inputStr.Split(' ');
-        if (fields.Length != 3) throw new Exception("Invalid input");
+        if (fields.Length != 3) throw new Exception("Za malo danych");
 
         int ut = -1;
         foreach (UserType type in Enum.GetValues(typeof(UserType)))
@@ -15,7 +15,7 @@ public static class InputParser
                 ut = (int) type;
             }
         }
-        if (ut == -1) throw new Exception("Invalid input");
+        if (ut == -1) throw new Exception("Nie istnieje taki typ uzytkownika");
         return new User(fields[0], fields[1], (UserType) ut);
     }
     
@@ -31,28 +31,28 @@ public static class InputParser
                 m = (int) manufacturer;
             }
         }
-        if (m == -1) throw new Exception("Invalid input");
+        if (m == -1) throw new Exception("Nie istnieje taki producent");
         
         switch  (fields[0])
         {
             case "Laptop":
-                if (fields.Length != 6) throw new Exception("Invalid input");
+                if (fields.Length != 6) throw new Exception("Za malo danych");
                 return new Laptop(fields[1], (Manufacturer) m, int.Parse(fields[3]), float.Parse(fields[4]), bool.Parse(fields[5]));
             case "Camera":
-                if (fields.Length != 6) throw new Exception("Invalid input");
+                if (fields.Length != 6) throw new Exception("Za malo danych");
                 return new Camera(fields[1], (Manufacturer) m, int.Parse(fields[3]), int.Parse(fields[4]), bool.Parse(fields[5]));
             case "Projector":
-                if (fields.Length != 6) throw new Exception("Invalid input");
+                if (fields.Length != 6) throw new Exception("Za malo danych");
                 return new Projector(fields[1], (Manufacturer) m, int.Parse(fields[3]), int.Parse(fields[4]), int.Parse(fields[5]));
             default:
-                throw new Exception("Invalid input");
+                throw new Exception("Nieznany typ urzadzenia");
         }
     }
     
     public static Rental StringToRental(String inputStr)
     {
         string[] fields = inputStr.Split(' ');
-        if (fields.Length != 4) throw new Exception("Invalid input");
+        if (fields.Length != 4) throw new Exception("Za malo danych");
         
         return new Rental(int.Parse(fields[0]), int.Parse(fields[1]), DateTime.Parse(fields[2]), DateTime.Parse(fields[3]));
     }
